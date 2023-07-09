a7d20596adda65fc4e191c77f907cad540b0e6f7
Reynolds398 <reynoldladestu@gmail.com>
2023-07-08 22:22:58 -0700

    Added Tetris Ghost in the game
    -Ghost block now visible for falling blocks
    -Changed layering so that Ghost block will be behind the actual block

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -1,18 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Ghost : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Tile tile;
+    public SpawnManager board;
+    public Pieces trackingPiece;
+
+    public Tilemap tilemap { get; private set; }
+    public Vector3Int[] cells { get; private set; }
+    public Vector3Int position { get; private set; }
+
+    private void Awake()
     {
-        
+        tilemap = GetComponentInChildren<Tilemap>();
+        cells = new Vector3Int[4];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        Clear();
+        Copy();
+        Drop();
+        Set();
+    }
+
+    private void Clear()
+    {
+        for (int i = 0; i < cells.Length; i++)
+        {
+            Vector3Int tilePosition = cells[i] + position;
+            tilemap.SetTile(tilePosition, null);
+        }
+    }
+
+    private void Copy()
+    {
+        for (int i = 0; i < cells.Length; i++)
+        {
+            cells[i] = trackingPiece.cells[i];
+        }
+    }
+
+    private void Drop()
+    {
+        for (int i = 0; i < cells.Length; i++)
+        {
+            Vector3Int position = trackingPiece.position;
+
+            int current = position.y;
+            int bottom = -board.boardSize.y / 2 - 1;
+
+            board.Clear(trackingPiece);
+
+            for (int row = current; row >= bottom; row--)
+            {
+                position.y = row;
+
+                if (board.IsValidPosition(trackingPiece, position))
+                {
+                    this.position = position;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            board.Set(trackingPiece);
+        }
+    }
+
+    private void Set()
+    {
+        for (int i = 0; i < cells.Length; i++)
+        {
+            Vector3Int tilePosition = cells[i] + position;
+            tilemap.SetTile(tilePosition, tile);
+        }
     }
 }
