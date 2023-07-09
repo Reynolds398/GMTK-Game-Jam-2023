4e9f45b3a318af9653e0e2bd1e7794ab37b768e5
Reynolds398 <reynoldladestu@gmail.com>
2023-07-09 05:28:52 -0700

    Added Rotations and Wall Kicks in the game
    -Added rotations and wall kicks in the game
    
    Notes:
    -Haven't tested so might have some bugs

--- Assets/Scripts/Pieces.cs
+++ Assets/Scripts/Pieces.cs
@@ -8,6 +8,7 @@ public class Pieces : MonoBehaviour
     public TetrominoData data { get; private set; }
     public Vector3Int[] cells { get; private set; }
     public Vector3Int position { get; private set; }
+    public int rotationIndex { get; private set; }
     public GameObject player;
 
     public float fallInterval = 1.0f;
@@ -67,6 +68,10 @@ public class Pieces : MonoBehaviour
         lockTime += Time.deltaTime;
 
         board.Clear(this);
+
+        //Rotate here
+        //1 is to the right
+        //-1 is to the left
 
         //Move left
         if (leftCount > 0)
@@ -154,6 +159,89 @@ public class Pieces : MonoBehaviour
         }
 
         return valid;
+    }
+
+    private void Rotate(int direction)
+    {
+        int originalRotation = rotationIndex;
+        rotationIndex = Wrap(rotationIndex + direction, 0, 4);
+
+        ApplyRotationMatrix(direction);
+
+        if (!TestWallKicks(rotationIndex, direction))
+        {
+            rotationIndex = originalRotation;
+            ApplyRotationMatrix(-direction);
+        }
+    }
+
+    private void ApplyRotationMatrix(int direction)
+    {
+        for (int i = 0; i < cells.Length; i++)
+        {
+            Vector3 cell = cells[i];
+
+            int x, y;
+
+            switch (data.tetromino)
+            {
+                case Tetromino.I:
+                case Tetromino.O:
+                    cell.x -= 0.5f;
+                    cell.y -= 0.5f;
+                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+
+                default:
+                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+            }
+
+            cells[i] = new Vector3Int(x, y, 0);
+        }
+    }
+
+    private bool TestWallKicks(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = GetWallKickIndex(rotationIndex, rotationDirection);
+
+        for (int i = 0; i < data.wallKicks.GetLength(1); i++)
+        {
+            Vector2Int translation = data.wallKicks[wallKickIndex, i];
+
+            if (Move(translation))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int GetWallKickIndex(int rotationIndex, int rotationDirection)
+    {
+        int wallKickIndex = rotationIndex * 2;
+
+        if (rotationDirection < 0)
+        {
+            wallKickIndex--;
+        }
+
+        return Wrap(wallKickIndex, 0, data.wallKicks.GetLength(0));
+    }
+
+    private int Wrap(int input, int min, int max)
+    {
+        if (input < min)
+        {
+            return max - (min - input) % (max - min);
+        }
+        else
+        {
+            return min + (input - min) % (max - min);
+        }
     }
 
     private void RaiseDifficulty()
